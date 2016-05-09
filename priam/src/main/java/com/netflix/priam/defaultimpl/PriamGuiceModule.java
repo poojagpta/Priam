563b04a486c7f0c3e79da0edd7476ccc03e1a33d
poojagpta <pooja.gupta@jbksoft.com>
2016-05-09 16:54:05 -0700

    Update PriamGuiceModule.java

--- priam/src/main/java/com/netflix/priam/defaultimpl/PriamGuiceModule.java
+++ priam/src/main/java/com/netflix/priam/defaultimpl/PriamGuiceModule.java
@@ -66,7 +66,7 @@ public class PriamGuiceModule extends AbstractModule
         bind(ICredentialGeneric.class).annotatedWith(Names.named("gcscredential")).to(GcsCredential.class);
         bind(ICredentialGeneric.class).annotatedWith(Names.named("pgpcredential")).to(PgpCredential.class);
         bind(IRestoreStrategy.class).annotatedWith(Names.named("encryptedrestore")).to(EncryptedRestoreStrategy.class);
-        bind(ICredential.class).to(IAMCredential.class);
+        bind(ICredential.class).to(ClearCredential.class);
         bind(IDeadTokenRetriever.class).to(DeadTokenRetriever.class);
         bind(IPreGeneratedTokenRetriever.class).to(PreGeneratedTokenRetriever.class);
         bind(INewTokenRetriever.class).to(NewTokenRetriever.class);
